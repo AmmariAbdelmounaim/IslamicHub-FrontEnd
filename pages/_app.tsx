97782f50891451ca1835e403796320260c639ccd
Ammari Abdelmounaim <abdelmounaim.ammari@gmail.com>
2023-07-03 16:28:53 +0100

    adding the logo in png format

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta charSet="UTF-8" />
-        <link rel="icon" type="image/svg+xml" href="/IslamicHub.svg" />
+        <link rel="icon" type="image/png" href="/IslamicHub.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
