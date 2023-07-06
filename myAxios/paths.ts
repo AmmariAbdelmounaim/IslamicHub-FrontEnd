9a29533418df5edae067a8a4429d952e6cffcc52
Ammari Abdelmounaim <abdelmounaim.ammari@gmail.com>
2023-07-06 18:56:29 +0100

    fixing paths

--- myAxios/paths.ts
+++ myAxios/paths.ts
@@ -1,3 +1,3 @@
-export const SIGN_UP_PATH = "users/auth/register";
-export const SIGN_IN_PATH = "users/auth/login";
-export const SIGN_OUT_PATH = "users/auth/signout";
+export const SIGN_UP_PATH = "api/users/auth/register";
+export const SIGN_IN_PATH = "api/users/auth/login";
+export const SIGN_OUT_PATH = "api/users/auth/signout";
