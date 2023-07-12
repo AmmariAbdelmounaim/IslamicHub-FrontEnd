64adf79fde31d6f5eeecc970091dbd2904ade276
Ammari Abdelmounaim <abdelmounaim.ammari@gmail.com>
2023-07-12 10:14:07 +0100

    modifying the store

--- redux/store.ts
+++ redux/store.ts
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import authReducer from "./features/authSlice";
+import { apiSlice } from "./features/apiSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  devTools: true,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
