3dbb3b8977451b5a4ecdd98a6f336c6755f998ec
Ammari Abdelmounaim <abdelmounaim.ammari@gmail.com>
2023-07-12 10:12:01 +0100

    adding rewrites for http requests

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
+module.exports = () => {
+  const rewrites = () => {
+    return [
+      {
+        source: "/authentication/signup",
+        destination: "http://localhost:8085/api/users/auth/register",
+      },
+    ];
+  };
+  return {
+    rewrites,
+  };
+};
