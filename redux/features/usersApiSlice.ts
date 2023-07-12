1c2228c2ed8771dd0e825273381636de88e2cc3c
Ammari Abdelmounaim <abdelmounaim.ammari@gmail.com>
2023-07-12 10:15:26 +0100

    adding userApiSlice

---
+++ redux/features/usersApiSlice.ts
@@ -0,0 +1,31 @@
+import { apiSlice } from "./apiSlice";
+
+const USERS_URL = " api/users/auth";
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/login`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    logout: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/logout`,
+        method: "DELETE",
+        body: data,
+      }),
+    }),
+    register: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/register`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation, useRegisterMutation, useLogoutMutation } =
+  usersApiSlice;
