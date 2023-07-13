5c6123cfd61b260b03bbb85c36c075eef2375c91
Ammari Abdelmounaim <abdelmounaim.ammari@gmail.com>
2023-07-13 16:10:06 +0100

    adding the auth redux to login page

--- components/authentication/logIn/index.tsx
+++ components/authentication/logIn/index.tsx
@@ -7,23 +7,32 @@ import {
   useFormikContext,
   FieldProps,
 } from "formik";
+import { useEffect } from "react";
 import * as Yup from "yup";
 import Link from "next/link";
 import FillButton from "../../button/FillButton";
+import { useAppDispatch, useAppSelector } from "../../../redux/store";
+import {
+  useLoginMutation,
+  useRegisterMutation,
+} from "../../../redux/features/usersApiSlice";
+import { setCredentials } from "../../../redux/features/authSlice";
+import Error from "next/error";
 
 interface FormValues {
   email: string;
   password: string;
 }
 export const LoginForm = () => {
-  // Async submit function
-  const onSubmit = async (
-    values: FormValues,
-    { setSubmitting }: FormikHelpers<FormValues>
-  ) => {
-    console.log(values);
-    setSubmitting(false);
-  };
+  //redux:
+  const dispatch = useAppDispatch();
+  const [login, { isLoading }] = useLoginMutation();
+  const { userInfo } = useAppSelector((state) => state.auth);
+  useEffect(() => {
+    if (userInfo) {
+      console.log("there is already a user (:");
+    }
+  }, [userInfo]);
 
   // Validation schema
   const validationSchema = Yup.object({
@@ -51,9 +60,7 @@ export const LoginForm = () => {
         >
           {label}
         </label>
-        <p className="text-red-600 font-medium">
-          <ErrorMessage name={name} />
-        </p>
+
         <Field
           type={type || "text"}
           name={name}
@@ -62,7 +69,6 @@ export const LoginForm = () => {
             display: "flex",
             height: "40px",
             padding: "14px 10px 15px 10px",
-            
           }}
           className={`
           flex h-[40px] pl-[10px] w-full rounded-lg 
@@ -74,6 +80,9 @@ export const LoginForm = () => {
           }
         `}
         />
+        <p className="text-red-600 font-medium">
+          <ErrorMessage name={name} />
+        </p>
       </div>
     );
   };
@@ -105,50 +114,63 @@ export const LoginForm = () => {
         </p>
       </div>
 
-        {/* Form */}
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={onSubmit}
-        >
-          <Form 
-          className="flex flex-col gap-[64px]"
-          >
-            {/* Troisieme div */}
-            <div className="flex flex-col gap-[24px]">
-              {/* 2 input fields for email and password */}
-              <CustomField
-                name="email"
-                label="Email"
-                placeholder="example@gmail.Com"
-              />
-              <CustomField
-                name="password"
-                label="Password"
-                placeholder="Enter your password"
-                type="password"
-              />
-            </div>
+      {/* Form */}
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={async (values, { setSubmitting }) => {
+          try {
+            const res = await login({
+              email: values.email,
+              password: values.password,
+            }).unwrap();
+            dispatch(setCredentials({ ...res }));
+            console.log("done :)");
+          } catch (err) {
+            console.log("error here");
+          }
+        }}
+      >
+        <Form className="flex flex-col gap-[64px]">
+          {/* Troisieme div */}
+          <div className="flex flex-col gap-[24px]">
+            {/* 2 input fields for email and password */}
+            <CustomField
+              name="email"
+              label="Email"
+              placeholder="example@gmail.Com"
+            />
+            <CustomField
+              name="password"
+              label="Password"
+              placeholder="Enter your password"
+              type="password"
+            />
+          </div>
 
-            {/* Quatrieme div */}
-            <div className="flex flex-col gap-[24px] justify-center">
-              {/* Sign in button */}
-              <FillButton
-                type="submit"
-                additionalStyle="w-[195px] h-[45px] flex items-center justify-center"
+          {/* Quatrieme div */}
+          <div className="flex flex-col gap-[24px] justify-center">
+            {/* Sign in button */}
+            <FillButton
+              type="submit"
+              additionalStyle="w-[195px] h-[45px] flex items-center justify-center"
+            >
+              Sign In
+            </FillButton>
+
+            {/* You have an account -> sign up <Link/> */}
+            <p className="font-poppins text-16 font-medium items-center">
+              Dont have an account?
+              <Link
+                href="/authentication/signup"
+                className="text-primary-orange-normal"
               >
-                Sign In
-              </FillButton>
-
-              {/* You have an account -> sign up <Link/> */}
-              <p className="font-poppins text-16 font-medium items-center">
-                Don't have an account?{" "}
-                <Link href="/authentication/signup" className="text-primary-orange-normal">Sign Up</Link>
-              </p>
-            </div>
-
-          </Form>
-        </Formik>
+                Sign Up
+              </Link>
+            </p>
+          </div>
+        </Form>
+      </Formik>
     </div>
   );
 };
