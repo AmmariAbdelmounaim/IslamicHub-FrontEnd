ae59ff37adcbd96cfc815ac6b52824a9ca2baf2f
Ammari Abdelmounaim <abdelmounaim.ammari@gmail.com>
2023-07-14 08:21:54 +0100

    post route for payment

---
+++ app/api/payment/route.ts
@@ -0,0 +1,35 @@
+import Stripe from "stripe";
+import { NextResponse, NextRequest } from "next/server";
+import { Price } from "../../pricing/page";
+
+export async function POST(request: Request) {
+  const stripe = new Stripe(
+    process.env.STRIPE_SECRET_KEY as string,
+    {} as Stripe.StripeConfig
+  );
+  let data: Price = await request.json();
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "usd",
+          product: data.product,
+          unit_amount: data.unit_amount,
+          recurring: {
+            interval: `${
+              data.nickname.startsWith("Monthly") ? "month" : "year"
+            }`, // 'month' | 'year'
+          },
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "subscription",
+    success_url: "http://localhost:3001/succ",
+    cancel_url: "http://localhost:3001/echec",
+  });
+
+  return NextResponse.json(session.url);
+}
