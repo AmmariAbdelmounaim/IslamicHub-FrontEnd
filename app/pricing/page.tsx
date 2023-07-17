e4ea46930d5f649a89c40d68786c222216d09e77
Ammari Abdelmounaim <abdelmounaim.ammari@gmail.com>
2023-07-17 16:10:43 +0100

    adding the navbar to the pricing page

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -2,6 +2,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import PricingCard from "../../components/Card/pricingCard";
+import Navbar from "../../components/navbar";
+import FillButton from "../../components/button/FillButton";
 
 interface Recurring {
   aggregate_usage: string | null;
@@ -53,64 +55,64 @@ export default function PricingPage() {
   };
 
   return (
-    <div className="py-[40px] px-[100px] flex flex-col gap-[40px] ">
-      <h1 className="font-sourceSerif text-[24px] text-center text-primary-brown-darker font-semibold capitalize sm:text-[28px] ">
-        Choose a plan that is right for your center
-      </h1>
-      <div>
-        <button></button>
-        <button></button>
+    <>
+      <Navbar />
+
+      <div className="py-[40px] px-[100px] flex flex-col gap-[40px] items-center">
+        <h1 className="font-sourceSerif text-[24px] text-center text-primary-brown-darker font-semibold capitalize sm:text-[28px] ">
+          Choose a plan that is right for your center
+        </h1>
+        <FillButton
+          onClick={toggleSubscriptionType}
+          additionalStyle="w-[400px] "
+        >
+          Switch to {subscriptionType === "monthly" ? "Yearly" : "Monthly"}{" "}
+          Subscription
+        </FillButton>
+        <div className="flex flex-col sm:flex-row gap-[50px] justify-center items-center">
+          {subscriptionType === "monthly" ? (
+            <>
+              {prices &&
+                prices.map((price) => {
+                  if (price.nickname.startsWith("Monthly")) {
+                    return (
+                      <PricingCard
+                        offer={price}
+                        key={price.id}
+                        monthly={true}
+                        title={price.nickname.split(" ")[1]}
+                        description="For Islamic centers"
+                        price={price.unit_amount / 100}
+                        features={["Customized landing page"]}
+                      />
+                    );
+                  }
+                  return null;
+                })}
+            </>
+          ) : (
+            <>
+              {prices &&
+                prices.map((price) => {
+                  if (price.nickname.startsWith("Yearly")) {
+                    return (
+                      <PricingCard
+                        offer={price}
+                        key={price.id}
+                        monthly={true}
+                        title={price.nickname.split(" ")[1]}
+                        description="For Islamic centers"
+                        price={price.unit_amount / 100 / 12}
+                        features={["Customized landing page"]}
+                      />
+                    );
+                  }
+                  return null;
+                })}
+            </>
+          )}
+        </div>
       </div>
-      <button
-        onClick={toggleSubscriptionType}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded self-center mb-4"
-      >
-        Switch to {subscriptionType === "monthly" ? "Yearly" : "Monthly"}{" "}
-        Subscription
-      </button>
-      <div className="flex flex-col sm:flex-row gap-[50px] justify-center items-center">
-        {subscriptionType === "monthly" ? (
-          <>
-            {prices &&
-              prices.map((price) => {
-                if (price.nickname.startsWith("Monthly")) {
-                  return (
-                    <PricingCard
-                      offer={price}
-                      key={price.id}
-                      monthly={true}
-                      title="Starter "
-                      description="For Islamic centers"
-                      price={price.unit_amount / 100}
-                      features={["Customized landing page"]}
-                    />
-                  );
-                }
-                return null;
-              })}
-          </>
-        ) : (
-          <>
-            {prices &&
-              prices.map((price) => {
-                if (price.nickname.startsWith("Yearly")) {
-                  return (
-                    <PricingCard
-                      offer={price}
-                      key={price.id}
-                      monthly={true}
-                      title="Starter "
-                      description="For Islamic centers"
-                      price={price.unit_amount / 100 / 12}
-                      features={["Customized landing page"]}
-                    />
-                  );
-                }
-                return null;
-              })}
-          </>
-        )}
-      </div>
-    </div>
+    </>
   );
 }
