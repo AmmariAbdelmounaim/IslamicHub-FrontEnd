f370a1850a7ecbe67a567705cea205bcc2278fa8
Ammari Abdelmounaim <abdelmounaim.ammari@gmail.com>
2023-07-17 16:11:32 +0100

    validation schema for login and sign up pages

---
+++ components/authentication/validationSchema.tsx
@@ -0,0 +1,29 @@
+import { isPossiblePhoneNumber } from "react-phone-number-input";
+import * as Yup from "yup";
+
+export const validationSchemaSignUpForm = Yup.object({
+  islamicCenterName: Yup.string().required("Islamic Center name is required"),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(8, "Password must be at least 8 characters")
+    .max(20, "Password must not exceed 20 characters")
+    .required("Password required")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+      "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+    ),
+  country: Yup.string().required("Country is required"),
+  address: Yup.string().required("Address is required"),
+  phoneNumber: Yup.string()
+    .test("phoneNumber", "Phone number is not valid", (value) =>
+      isPossiblePhoneNumber(value ?? "")
+    )
+    .required("Phone number is required"),
+});
+
+export const validationSchemaLoginForm = Yup.object({
+  email: Yup.string().required("Email is required"),
+  password: Yup.string().required("Password is required"),
+});
