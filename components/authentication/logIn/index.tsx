93685611630cbddd32bc20bf5aaf6620d64bc20a
Ammari Abdelmounaim <abdelmounaim.ammari@gmail.com>
2023-07-17 16:12:07 +0100

    removing the old custom inputs and importing them instead

--- components/authentication/logIn/index.tsx
+++ components/authentication/logIn/index.tsx
@@ -1,23 +1,13 @@
-import {
-  Formik,
-  Field,
-  Form,
-  ErrorMessage as FormikErrorMessage,
-  FormikHelpers,
-  useFormikContext,
-  FieldProps,
-} from "formik";
+import { Formik, Form } from "formik";
 import { useEffect } from "react";
-import * as Yup from "yup";
 import Link from "next/link";
 import FillButton from "../../button/FillButton";
 import { useAppDispatch, useAppSelector } from "../../../redux/store";
-import {
-  useLoginMutation,
-  useRegisterMutation,
-} from "../../../redux/features/usersApiSlice";
+import { useLoginMutation } from "../../../redux/features/usersApiSlice";
 import { setCredentials } from "../../../redux/features/authSlice";
-import Error from "next/error";
+import { useRouter } from "next/navigation";
+import { CustomField } from "../../formInputs/customField";
+import { validationSchemaLoginForm } from "../validationSchema";
 
 interface FormValues {
   email: string;
@@ -26,73 +16,11 @@ interface FormValues {
 export const LoginForm = () => {
   //redux:
   const dispatch = useAppDispatch();
-  const [login, { isLoading }] = useLoginMutation();
   const { userInfo } = useAppSelector((state) => state.auth);
-  useEffect(() => {
-    if (userInfo) {
-      console.log("there is already a user (:");
-    }
-  }, [userInfo]);
+  const [login, { isLoading }] = useLoginMutation();
+  const router = useRouter();
 
-  // Validation schema
-  const validationSchema = Yup.object({
-    email: Yup.string().required("Email is required"),
-    password: Yup.string().required("Password is required"),
-  });
-
-  // Custom field
-  const CustomField: React.FC<{
-    name: string;
-    label: string;
-    placeholder: string;
-    type?: string;
-  }> = ({ name, label, placeholder, type }) => {
-    const { errors, touched } = useFormikContext<FormValues>(); // Access formik context
-    const hasError = Boolean(
-      touched[name as keyof FormValues] && errors[name as keyof FormValues]
-    );
-
-    return (
-      <div>
-        <label
-          htmlFor={name}
-          className="font-poppins capitalize text-[18px] text-secondary-brown-dark"
-        >
-          {label}
-        </label>
-
-        <Field
-          type={type || "text"}
-          name={name}
-          placeholder={placeholder}
-          style={{
-            display: "flex",
-            height: "40px",
-            padding: "14px 10px 15px 10px",
-          }}
-          className={`
-          flex h-[40px] pl-[10px] w-full rounded-lg 
-          font-poppins text-[16px] font-medium placeholder:text-color-placeholder
-          ${
-            hasError
-              ? "border-2 border-red-600"
-              : "border-2 border-secondary-brown-normal "
-          }
-        `}
-        />
-        <p className="text-red-600 font-medium">
-          <ErrorMessage name={name} />
-        </p>
-      </div>
-    );
-  };
-
-  // Custom Error Message
-  const ErrorMessage: React.FC<{ name: string }> = ({ name }) => (
-    <FormikErrorMessage name={name}>
-      {(msg) => <div style={{ color: "red" }}>{msg}</div>}
-    </FormikErrorMessage>
-  );
+  useEffect(() => {}, [userInfo]);
 
   const initialValues: FormValues = {
     email: "",
@@ -100,77 +28,67 @@ export const LoginForm = () => {
   };
 
   return (
-    <div className="flex flex-col gap-[64px]">
-      {/* Premiere div */}
+    <>
       <div className="flex flex-col gap-[16px]">
-        {/* Header */}
-        <h1 className="text-[28px] font-sourceSerif font-semibold">
+        <h1 className=" capitalize text-secondary-brown-darker font-sourceSerif text-[32px] font-normal">
           Welcome Back!
         </h1>
-
-        {/* Quote */}
-        <p className="font-poppins text-[18px] font-normal text-secondary-brown-normal">
+        <p className="capitalize font-poppins text-[16px] font-normal text-secondary-brown-normal">
           Quote
         </p>
       </div>
 
-      {/* Form */}
       <Formik
         initialValues={initialValues}
-        validationSchema={validationSchema}
+        validationSchema={validationSchemaLoginForm}
         onSubmit={async (values, { setSubmitting }) => {
           try {
             const res = await login({
               email: values.email,
               password: values.password,
             }).unwrap();
+            console.log("login response", res);
             dispatch(setCredentials({ ...res }));
-            console.log("done :)");
-          } catch (err) {
-            console.log("error here");
-          }
+            router.push("/welcome");
+          } catch (err) {}
         }}
       >
-        <Form className="flex flex-col gap-[64px]">
-          {/* Troisieme div */}
-          <div className="flex flex-col gap-[24px]">
-            {/* 2 input fields for email and password */}
-            <CustomField
-              name="email"
-              label="Email"
-              placeholder="example@gmail.Com"
-            />
-            <CustomField
-              name="password"
-              label="Password"
-              placeholder="Enter your password"
-              type="password"
-            />
-          </div>
+        {({}) => (
+          <Form>
+            <div className="my-[40px] flex flex-col gap-[32px]">
+              <CustomField<FormValues>
+                name="email"
+                label="Email"
+                placeholder="example@gmail.com"
+              />
+              <CustomField<FormValues>
+                name="password"
+                label="Password"
+                placeholder="Enter your password"
+                type="password"
+              />
+            </div>
 
-          {/* Quatrieme div */}
-          <div className="flex flex-col gap-[24px] justify-center">
-            {/* Sign in button */}
-            <FillButton
-              type="submit"
-              additionalStyle="w-[195px] h-[45px] flex items-center justify-center"
-            >
-              Sign In
-            </FillButton>
-
-            {/* You have an account -> sign up <Link/> */}
-            <p className="font-poppins text-16 font-medium items-center">
-              Dont have an account?
-              <Link
-                href="/authentication/signup"
-                className="text-primary-orange-normal"
+            <div className="flex flex-col gap-[32px] justify-center items-center">
+              <FillButton
+                type="submit"
+                additionalStyle="w-[195px] h-[45px] flex items-center justify-center"
               >
-                Sign Up
-              </Link>
-            </p>
-          </div>
-        </Form>
+                Sign In
+              </FillButton>
+              <p className="capitalize font-poppins text-[16px]">
+                Dont have an account?{" "}
+                <Link
+                  href="/authentication/signup"
+                  className="text-primary-orange-normal"
+                >
+                  Sign Up
+                </Link>
+              </p>
+            </div>
+          </Form>
+        )}
       </Formik>
-    </div>
+    </>
   );
 };
--- components/authentication/signUp/index.tsx
+++ components/authentication/signUp/index.tsx
@@ -1,26 +1,18 @@
 "use client";
 
-import {
-  Formik,
-  Field,
-  Form,
-  ErrorMessage as FormikErrorMessage,
-  useFormikContext,
-} from "formik";
+import { Formik, Form } from "formik";
 import { useEffect } from "react";
 import { useRegisterMutation } from "../../../redux/features/usersApiSlice";
 import { setCredentials } from "../../../redux/features/authSlice";
-import * as Yup from "yup";
 import FillButton from "../../button/FillButton";
-import countries from "i18n-iso-countries";
-import enLocale from "i18n-iso-countries/langs/en.json";
 import "react-phone-number-input/style.css";
-import PhoneInput from "react-phone-number-input";
-import { isPossiblePhoneNumber } from "react-phone-number-input";
-import { getCountries, getCountryCallingCode } from "libphonenumber-js";
 import { useAppDispatch, useAppSelector } from "../../../redux/store";
-
-countries.registerLocale(enLocale);
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { CustomField } from "../../formInputs/customField";
+import { CustomSelectField } from "../../formInputs/customSelectField";
+import { CustomPhoneInput } from "../../formInputs/customPhoneInput";
+import { validationSchemaSignUpForm } from "../validationSchema";
 
 interface FormValues {
   islamicCenterName: string;
@@ -31,196 +23,7 @@ interface FormValues {
   phoneNumber: string;
 }
 
-// Validation schema
-const validationSchema = Yup.object({
-  islamicCenterName: Yup.string().required("Islamic Center name is required"),
-  email: Yup.string()
-    .email("Invalid email address")
-    .required("Email is required"),
-  password: Yup.string()
-    .min(8, "Password must be at least 8 characters")
-    .max(20, "Password must not exceed 20 characters")
-    .required("Password required")
-    .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
-      "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
-    ),
-  country: Yup.string().required("Country is required"),
-  address: Yup.string().required("Address is required"),
-  phoneNumber: Yup.string()
-    .test("phoneNumber", "Phone number is not valid", (value) =>
-      isPossiblePhoneNumber(value ?? "")
-    )
-    .required("Phone number is required"),
-});
-
-// Custom Field
-const CustomField: React.FC<{
-  name: string;
-  label: string;
-  placeholder: string;
-  type?: string;
-}> = ({ name, label, placeholder, type }) => {
-  const { errors, touched } = useFormikContext<FormValues>(); // access formik context
-  const hasError = Boolean(
-    touched[name as keyof FormValues] && errors[name as keyof FormValues]
-  );
-
-  return (
-    <div>
-      <label
-        htmlFor={name}
-        className="font-poppins capitalize text-[18px] text-secondary-brown-dark"
-      >
-        {label}
-      </label>
-      <ErrorMessage name={name} />
-      <Field
-        type={type || "text"}
-        name={name}
-        placeholder={placeholder}
-        className={`
-          w-full flex h-[40px] pl-[16px] items-center rounded-lg 
-          font-poppins text-[16px] font-medium placeholder:text-color-placeholder
-          ${
-            hasError
-              ? "border-2 border-red-600"
-              : "border-2 border-secondary-brown-normal placeholder:capitalize"
-          }
-        `}
-      />
-    </div>
-  );
-};
-
-// Custom Select Field
-const CustomSelectField: React.FC<{ name: string; label: string }> = ({
-  name,
-  label,
-}) => {
-  const { errors, touched } = useFormikContext<FormValues>(); // access formik context
-  const hasError = Boolean(
-    touched[name as keyof FormValues] && errors[name as keyof FormValues]
-  ); // check if this field has an error
-
-  const countryObj = countries.getNames("en", { select: "official" });
-  const countryArr = Object.entries(countryObj).map(([key, value]) => ({
-    label: value,
-    value: key,
-  }));
-
-  return (
-    <div>
-      <label
-        htmlFor={name}
-        className="font-poppins capitalize text-[18px] text-secondary-brown-dark"
-      >
-        {label}
-      </label>
-      <ErrorMessage name={name} />
-      <div className="relative">
-        <Field
-          as="select"
-          name={name}
-          className={`
-            w-full appearance-none h-[40px] pl-[16px] pr-[30px] items-center rounded-lg 
-            font-poppins text-[16px] font-medium placeholder:text-color-placeholder
-            ${
-              hasError
-                ? "border-2 border-red-600"
-                : "border-2 border-secondary-brown-normal placeholder:capitalize"
-            }
-          `}
-        >
-          <option value="">Select your country</option>
-          {countryArr.map((country) => (
-            <option key={country.value} value={country.label}>
-              {country.label}
-            </option>
-          ))}
-        </Field>
-        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2">
-          <svg
-            className=" h-4 w-4 fill-secondary-brown-normal"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-          >
-            <path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.787-0.335c0 0-4.287-4.084-4.695-4.502s0.062-1.17 0-1.615z" />
-          </svg>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-// Custom Error Message
-const ErrorMessage: React.FC<{ name: string }> = ({ name }) => (
-  <FormikErrorMessage name={name}>
-    {(msg) => <p className="text-red-600 font-poppins font-medium">{msg}</p>}
-  </FormikErrorMessage>
-);
-
-// Custom Phone Input
-
-const CustomPhoneInput: React.FC<{ name: string; label: string }> = ({
-  name,
-  label,
-}) => {
-  const { setFieldValue, values, errors, touched } =
-    useFormikContext<FormValues>();
-  const hasError = Boolean(
-    touched[name as keyof FormValues] && errors[name as keyof FormValues]
-  );
-
-  return (
-    <div>
-      <label
-        htmlFor={name}
-        className="font-poppins capitalize text-[18px] text-secondary-brown-dark"
-      >
-        {label}
-      </label>
-      <ErrorMessage name={name} />
-      <PhoneInput
-        international={false}
-        placeholder="Enter Your Phone Number"
-        defaultCountry="MA"
-        value={values[name as keyof FormValues]}
-        onChange={(value) => setFieldValue(name, value)}
-        className={`
-          w-full flex h-[40px] pl-[16px] items-center rounded-lg 
-          font-poppins text-[16px] font-medium placeholder:text-color-placeholder
-          ${
-            hasError
-              ? "border-2 border-red-600"
-              : "border-2 border-secondary-brown-normal placeholder:capitalize"
-          }
-        `}
-      />
-    </div>
-  );
-};
-
-const CountrySelectWithCodes: React.FC<any> = ({ value, onChange }) => {
-  const countryOptions = getCountries().map((countryCode) => {
-    const callingCode = getCountryCallingCode(countryCode);
-    return (
-      <option key={countryCode} value={countryCode}>
-        {countryCode}+{callingCode}
-      </option>
-    );
-  });
-
-  return (
-    <select value={value} onChange={onChange}>
-      {countryOptions}
-    </select>
-  );
-};
-
 const SignUpForm: React.FC = () => {
-  countries.registerLocale(enLocale);
-
   const initialValues: FormValues = {
     islamicCenterName: "",
     email: "",
@@ -234,12 +37,8 @@ const SignUpForm: React.FC = () => {
   const dispatch = useAppDispatch();
   const { userInfo } = useAppSelector((state) => state.auth);
   const [register, { isLoading }] = useRegisterMutation();
+  const router = useRouter();
 
-  useEffect(() => {
-    if (userInfo) {
-      console.log("there is already a user (:");
-    }
-  }, [userInfo]);
   return (
     <>
       <div className="flex flex-col gap-[16px]">
@@ -253,54 +52,51 @@ const SignUpForm: React.FC = () => {
 
       <Formik
         initialValues={initialValues}
-        validationSchema={validationSchema}
+        validationSchema={validationSchemaSignUpForm}
         onSubmit={async (values, { setSubmitting }) => {
           try {
+            console.log("bdinal");
             const res = await register({
               firstname: values.islamicCenterName,
-              lastname: "",
+              lastname: "test",
               email: values.email,
               tele: values.phoneNumber,
               ville: values.country,
               adresse: values.address,
               password: values.password,
             }).unwrap();
+            console.log("success");
+            console.log("signUP response", res);
             dispatch(setCredentials({ ...res }));
+            console.log("success");
+            // router.push("/verification");
           } catch (err) {
-            console.log("error");
+            console.log(err);
           }
         }}
       >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-        }) => (
+        {({ isSubmitting }) => (
           <Form>
             <div className="my-[40px] flex flex-col gap-[32px]">
-              <CustomField
+              <CustomField<FormValues>
                 name="islamicCenterName"
                 label="Islamic Center Name"
                 placeholder="The Islamic Center Name"
               />
-              <CustomField
+              <CustomField<FormValues>
                 type="email"
                 name="email"
                 label="Email"
                 placeholder="Example@gmail.com"
               />
-              <CustomField
+              <CustomField<FormValues>
                 type="password"
                 name="password"
                 label="Password"
                 placeholder="Enter your password"
               />
               <CustomSelectField name="country" label="Country" />
-              <CustomField
+              <CustomField<FormValues>
                 name="address"
                 label="Address"
                 placeholder="Enter your address"
@@ -318,7 +114,12 @@ const SignUpForm: React.FC = () => {
               </FillButton>
               <p className="capitalize font-poppins text-[16px]">
                 you have an account?{" "}
-                <span className="text-primary-orange-normal">Sign In</span>
+                <Link
+                  href="/authentication/login"
+                  className="text-primary-orange-normal"
+                >
+                  Sign In
+                </Link>
               </p>
             </div>
           </Form>
