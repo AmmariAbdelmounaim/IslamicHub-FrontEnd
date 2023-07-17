5c92b885a4e3527c459866c775df004d8fa8b25b
Ammari Abdelmounaim <abdelmounaim.ammari@gmail.com>
2023-07-17 16:12:45 +0100

    adding the middleware

--- redux/store.ts
+++ redux/store.ts
@@ -2,14 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import authReducer from "./features/authSlice";
 import { apiSlice } from "./features/apiSlice";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
 });
+
+setupListeners(store.dispatch);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
