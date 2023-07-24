fb80d69bd6902fb7449ac15cf488ff12e0ecfe47
Ammari Abdelmounaim <abdelmounaim.ammari@gmail.com>
2023-07-24 17:08:18 +0100

    cms page

--- app/[locale]/cms/page.tsx
+++ app/[locale]/cms/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 import SidebarWithNavbar from "../../../components/cmsNavbar";
-
-export default function TestPage() {
-  return <SidebarWithNavbar />;
+import CsmSidebar from "../../../components/cmsNavbar/sidebar";
+import { SketchPicker } from "react-color";
+import ColorPicker from "../../../components/cms/colorPicker";
+import CmsNavbar from "../../../components/cmsNavbar/navbar";
+export default function CmsPage({}) {
+  return <CsmSidebar />;
 }
