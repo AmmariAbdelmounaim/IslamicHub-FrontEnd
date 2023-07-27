093658640029edf6f73426fa9e46713a4d75c81a
Ammari Abdelmounaim <abdelmounaim.ammari@gmail.com>
2023-07-27 10:55:03 +0100

    change style when navbar link is active

--- components/cmsNavbar/sidebar.tsx
+++ components/cmsNavbar/sidebar.tsx
@@ -2,8 +2,15 @@ import Image from "next/image";
 import { INavItemCms, NavListCms } from "../../helpers/consts";
 import Link from "next/link";
 import { ReactSVG } from "react-svg";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 export const CsmSidebar = () => {
+  const pathname = usePathname();
+  useEffect(() => {
+    console.log(pathname);
+  }, []);
+
   return (
     <div className="h-screen w-[280px] flex flex-col py-[32px] gap-[56px] bg-secondary-brown-light-active ">
       <div className="flex justify-start px-[16px]">
@@ -22,17 +29,25 @@ export const CsmSidebar = () => {
             <li key={item.title}>
               <Link
                 href={item.path}
-                className="group flex gap-[10px]  px-[16px] py-[8px] hover:bg-[#CE7D39] active:bg-[#CE7D39]  "
+                className={`group flex gap-[10px]  px-[16px] py-[8px] ${
+                  pathname === item.path ? "bg-[#CE7D39]" : ""
+                } hover:bg-[#CE7D39] active:bg-[#CE7D39]`}
               >
                 <ReactSVG
                   src={item.icon}
-                  className="svg-icon group-hover:stroke-white group-hover:fill-white  stroke-[#362A1C] fill-[#362A1C]"
+                  className={`svg-icon group-hover:stroke-white group-hover:fill-white ${
+                    pathname === item.path && "stroke-white fill-white"
+                  }  stroke-[#362A1C] fill-[#362A1C]`}
                   beforeInjection={(svg) => {
                     svg.setAttribute("style", "width: 24px; height: 24px");
                   }}
                 />
 
-                <p className="font-poppins capitalize font-[18px] text-secondary-brown-darker group-hover:text-white">
+                <p
+                  className={`font-poppins capitalize font-[18px] text-secondary-brown-darker ${
+                    pathname === item.path && "text-white"
+                  } group-hover:text-white`}
+                >
                   {item.title}
                 </p>
               </Link>
