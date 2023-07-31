c7c4f89a76a85f711585ba99925ff7de5c71cb11
Ammari Abdelmounaim <abdelmounaim.ammari@gmail.com>
2023-07-31 16:59:26 +0100

    review page

---
+++ app/[locale]/preview/page.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function PreviewPage() {
+  return (
+    <div className="flex flex-col">
+      <nav>
+        {/* hna radir navbar ou tkoun fixed (bach la scrollina lte7t hiya teb9a flfou9 ;) ) */}
+      </nav>
+      <div>
+        {/* hna rayoukoun slider lifih douk les images btlata i guess kitssawerb breact-slick check it  */}
+      </div>
+      <div>{/* la barre dyal donation ri provisoire ousafi */}</div>
+      <div>{/* about us oudakchi */}</div>
+      <div>
+        {/* upcoming events (rah l event componenent deja saybtou zid 3lih props bach tbedel ftest litema) */}
+      </div>
+      <div>{/* prayer times (dir ri tableau) */}</div>
+      <div>{/* our services dir 3 cards  */}</div>
+      <div>{/* donation section */}</div>
+      <div>{/* testimonials */}</div>
+      <div>{/* get intouch */}</div>
+      <div>{/* footer */}</div>
+    </div>
+  );
+}
+
+export default PreviewPage;
