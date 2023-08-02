198a6bc96d1acad1be4d1e17aeb879bd5b8de7d4
ChaouiAli <a.echchaoui@esisa.ac.ma>
2023-08-02 20:13:45 +0100

    upcomingEvents

---
+++ app/[locale]/preview/upcomingEvents.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+function UpCE() {
+  return (
+    <div className="flex flex-col gap-[40px] mx-[100px] py-[64px] border-b-[2px] border-b-secondary-brown-light-active ">
+      <div className="flex flex-col gap-[56px]">
+        <div className="flex justify-between ">
+          <h3 className="text-primary-orange-darker font-sourceSerif text-[32px] font-normal w-[197px] h-[40px] ">
+            Jun <br /> 21
+          </h3>
+          <div>
+            <h1 className="text-primary-orange-darker font-semibold text-[28px] font-sourceSerif mb-3">
+              Spiritual Reflections: An Evening of Quranic Wisdom
+            </h1>
+            <p className="text-primary-orange-dark-active font-normal font-poppins text-[18px]">
+              Join us for a transformative evening of Quranic wisdom and spiritual reflections. Open to all, deepen your understanding and strengthen your connection to the Divine.
+            </p>
+            <p className="text-secondary-brown-normal font-semibold font-poppins text-[20px] mt-6">
+              16:00 - 20:00 <br /> Al Noor Hall, Crescent Street, Harmonyville
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-[56px]">
+        <div className="flex justify-between">
+          <h3 className="text-primary-orange-darker font-sourceSerif text-[32px] font-normal w-[197px] h-[40px] ">
+            Aug <br /> 19
+          </h3>
+          <div>
+            <h1 className="text-primary-orange-darker font-semibold text-[28px] font-sourceSerif mb-3">
+              Community Iftar: Embracing the Spirit of Giving
+            </h1>
+            <p className="text-primary-orange-dark-active font-normal font-poppins text-[18px]">
+              Come together in unity and generosity for our Community Iftar. Witness the power of collective giving as we share a delightful meal and extend a helping hand to those in need.
+            </p>
+            <p className="text-secondary-brown-normal font-semibold font-poppins text-[20px] mt-6">
+              20:00 - 22:00 <br /> Unity Park, Peaceful Avenue, Serenity City
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-[56px]">
+        <div className="flex justify-between">
+          <h3 className="text-primary-orange-darker font-sourceSerif text-[32px] font-normal w-[197px] h-[40px] ">
+            Sep <br /> 22
+          </h3>
+          <div>
+            <h1 className="text-primary-orange-darker font-semibold text-[28px] font-sourceSerif mb-3">
+              Youth Empowerment Workshop: Unleashing Potential
+            </h1>
+            <p className="text-primary-orange-dark-active font-normal font-poppins text-[18px]">
+              Calling all youth! Join motivational speakers for a journey of self-discovery and growth. Gain valuable skills, confidence, and unlock your leadership potential.
+            </p>
+            <p className="text-secondary-brown-normal font-semibold font-poppins text-[20px] mt-6">
+              11:00 - 13:00 <br /> Inspirational Center, Hope Street, Progressville
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UpCE;
