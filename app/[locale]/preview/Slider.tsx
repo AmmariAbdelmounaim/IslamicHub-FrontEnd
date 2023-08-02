e157e52698d26da68ea6bdd585c8d56728302396
Ammari Abdelmounaim <abdelmounaim.ammari@gmail.com>
2023-08-02 09:58:04 +0100

    slider componenet for the preview page

--- app/[locale]/preview/Slider.tsx
+++ app/[locale]/preview/Slider.tsx
@@ -1,45 +1,81 @@
-import React from "react";
+"use client";
+import React, { RefObject, useRef } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Image from "next/image";
+import DirectionButton from "../../../components/button/DirectionButton";
 
-const Slide = () => {
+function SliderImage() {
+  const sliderRef: RefObject<Slider> = React.useRef(null);
+
+  const NextArrow = ({ onClick }: { onClick?: () => void }) => {
+    return (
+      <div
+        className="z-50 right-0 flex items-center justify-end cursor-pointer"
+        onClick={onClick}
+      >
+        <DirectionButton left={false} />
+      </div>
+    );
+  };
+
+  const PrevArrow = ({ onClick }: { onClick?: () => void }) => {
+    return (
+      <div
+        className="z-50 left-0 flex items-center cursor-pointer"
+        onClick={onClick}
+      >
+        <DirectionButton left />
+      </div>
+    );
+  };
+
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: true,
   };
 
   return (
-    <div className="w-full h-[400px]">
-      <Slider {...settings}>
-        <div className="h-[400px]">
-          <img
+    <div className="w-full min-h-[800px] relative">
+      <Slider ref={sliderRef} className="z-30" {...settings}>
+        <div className="h-full w-full">
+          <Image
             src="/assets/heroimg.jpg"
             alt="Photo 1"
-            className="object-cover h-full w-full"
+            className="w-full"
+            width={1000}
+            height={800}
           />
         </div>
-        <div className="h-[400px]">
-          <img
+        <div className="h-full w-full">
+          <Image
             src="/assets/heroimg.jpg"
             alt="Photo 2"
-            className="object-cover h-full w-full"
+            className="w-full"
+            width={1000}
+            height={800}
           />
         </div>
-        <div className="h-[400px]">
-          <img
+        <div className="h-full w-full">
+          <Image
             src="/assets/heroimg.jpg"
             alt="Photo 3"
-            className="object-cover h-full w-full"
+            className="w-full"
+            width={1000}
+            height={800}
           />
         </div>
       </Slider>
+      <div className="h-full w-full absolute top-0 left-0 right-0 z-40 flex p-[100px] items-center justify-between">
+        <PrevArrow onClick={() => sliderRef.current?.slickPrev()} />
+        <NextArrow onClick={() => sliderRef.current?.slickNext()} />
+      </div>
     </div>
   );
-};
+}
 
-export default Slide;
+export default SliderImage;
