0d09ba41155133827ffeaef783ab78dce1b57176
Ammari Abdelmounaim <abdelmounaim.ammari@gmail.com>
2023-08-02 13:41:51 +0100

    add event button

--- app/[locale]/cms/calendar/page.tsx
+++ app/[locale]/cms/calendar/page.tsx
@@ -2,42 +2,34 @@
 import React from "react";
 import Event from "../../../../components/cms/event";
 import ColorPicker from "../../../../components/cms/colorPicker";
-import { Form, Formik } from "formik";
+import { FieldArray, Form, Formik } from "formik";
 import FillButton from "../../../../components/button/FillButton";
 import EventCard from "../../../../components/cms/eventCard";
 import { useTranslations } from "next-intl";
 
 interface FormValues {
-  firsteventName: string;
-  firsteventDescription: string;
-  firsteventDate: string;
-  firsteventLocation: string;
-  secondeventName: string;
-  secondeventDescription: string;
-  secondeventDate: string;
-  secondeventLocation: string;
-  thirdeventName: string;
-  thirdeventDescription: string;
-  thirdeventDate: string;
-  thirdeventLocation: string;
+  events: {
+    eventName: string;
+    eventDescription: string;
+    eventDate: string;
+    eventLocation: string;
+    eventTime: string;
+  }[];
   borderColor: string;
   backgroundColor: string;
   additionalInfoColor: string;
 }
 
 const initialValues: FormValues = {
-  firsteventName: "",
-  firsteventDescription: "",
-  firsteventDate: "",
-  firsteventLocation: "",
-  secondeventName: "",
-  secondeventDescription: "",
-  secondeventDate: "",
-  secondeventLocation: "",
-  thirdeventName: "",
-  thirdeventDescription: "",
-  thirdeventDate: "",
-  thirdeventLocation: "",
+  events: [
+    {
+      eventName: "",
+      eventDescription: "",
+      eventDate: "",
+      eventLocation: "",
+      eventTime: "",
+    },
+  ],
   borderColor: "#362A1C",
   backgroundColor: "#F8ECE1",
   additionalInfoColor: "#CE7D39",
@@ -55,7 +47,7 @@ export default function CalendarManagement() {
         }, 400);
       }}
     >
-      {({ isSubmitting }) => (
+      {({ values }) => (
         <Form>
           <div className="flex flex-col gap-[40px] pb-[20px]">
             {/* introduction */}
@@ -104,18 +96,54 @@ export default function CalendarManagement() {
             </div>
 
             {/* event information */}
-            <div className="px-[16px] pt-[16px] pb-[32px] flex flex-col gap-[40px] border-solid border-[1px] border-secondary-brown-normal-30-opacity rounded-[10px] ">
+            <div className="px-[16px] pt-[16px] pb-[32px] flex flex-col gap-[40px] border-solid border-[1px] border-secondary-brown-normal-30-opacity rounded-[10px]  ">
               <div className="flex flex-col border-b-[1px] border-b-secondary-brown-normal-30-opacity">
                 <p className="font-poppins capitalize text-[20px] text-secondary-brown-darker font-semibold mb-[10px]">
                   event information
                 </p>
               </div>
-              {/* fist event */}
-              <EventCard title="first event" />
-              {/* second event */}
-              <EventCard title="second event" />
-              {/* third event */}
-              <EventCard title="third event" withoutBorder />
+              <FieldArray
+                name="events"
+                render={(arrayHelpers) => (
+                  <div className="flex flex-col ">
+                    {values.events.map((event, index) => (
+                      <EventCard
+                        key={index}
+                        title={`event ${index + 1}`}
+                        index={index}
+                        arrayHelpers={arrayHelpers}
+                      />
+                    ))}
+                    <div className="flex w-full items-center justify-center ">
+                      <FillButton
+                        onClick={() =>
+                          arrayHelpers.push({
+                            eventName: "",
+                            eventDescription: "",
+                            eventDate: "",
+                            eventLocation: "",
+                            eventTime: "",
+                          })
+                        }
+                        additionalStyle="mt-[27px] gap-[10px] w-[250px]"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          x="0px"
+                          y="0px"
+                          width="30"
+                          fill="white"
+                          height="30"
+                          viewBox="0 0 128 128"
+                        >
+                          <path d="M 64 6.0507812 C 49.15 6.0507812 34.3 11.7 23 23 C 0.4 45.6 0.4 82.4 23 105 C 34.3 116.3 49.2 122 64 122 C 78.8 122 93.7 116.3 105 105 C 127.6 82.4 127.6 45.6 105 23 C 93.7 11.7 78.85 6.0507812 64 6.0507812 z M 64 12 C 77.3 12 90.600781 17.099219 100.80078 27.199219 C 121.00078 47.499219 121.00078 80.500781 100.80078 100.80078 C 80.500781 121.10078 47.500781 121.10078 27.300781 100.80078 C 7.0007813 80.500781 6.9992188 47.499219 27.199219 27.199219 C 37.399219 17.099219 50.7 12 64 12 z M 64 42 C 62.3 42 61 43.3 61 45 L 61 61 L 45 61 C 43.3 61 42 62.3 42 64 C 42 65.7 43.3 67 45 67 L 61 67 L 61 83 C 61 84.7 62.3 86 64 86 C 65.7 86 67 84.7 67 83 L 67 67 L 83 67 C 84.7 67 86 65.7 86 64 C 86 62.3 84.7 61 83 61 L 67 61 L 67 45 C 67 43.3 65.7 42 64 42 z"></path>
+                        </svg>
+                        Add Event
+                      </FillButton>
+                    </div>
+                  </div>
+                )}
+              />
             </div>
             <div className="flex items-center justify-center">
               <FillButton additionalStyle="px-[40px] py-[12px] flex gap-[12px]">
--- components/cms/eventCard.tsx
+++ components/cms/eventCard.tsx
@@ -3,13 +3,18 @@ import React from "react";
 import FillButton from "../button/FillButton";
 import { CustomField } from "../formInputs/customField";
 import DatePicker from "./datePicker";
+import { FieldArrayRenderProps } from "formik";
 
 export default function EventCard({
   title,
+  index,
   withoutBorder = false,
+  arrayHelpers,
 }: {
   title: string;
+  index: number;
   withoutBorder?: boolean;
+  arrayHelpers: FieldArrayRenderProps;
 }) {
   return (
     <div
@@ -27,12 +32,12 @@ export default function EventCard({
             label="event name"
             placeholder="Enter The Name Of Your Event"
             cms
-            name={`${title.split(" ")[0]}eventName`}
+            name={`events.${index}.eventName`}
           />
           <DatePicker
             label="event date"
             placeholder="Enter The Date of Your Event"
-            name={`${title.split(" ")[0]}eventDate`}
+            name={`events.${index}.eventDate`}
           />
         </div>
         <div className="flex gap-[64px]">
@@ -40,13 +45,13 @@ export default function EventCard({
             label="event location"
             placeholder="Enter The Location Of Your Event"
             cms
-            name={`${title.split(" ")[0]}eventLocation`}
+            name={`events.${index}.eventLocation`}
           />
           <CustomField
             label="event time"
             placeholder="Enter The Time Of Your Event"
             cms
-            name={`${title.split(" ")[0]}eventTime`}
+            name={`events.${index}.eventTime`}
           />
         </div>
         <div className="flex items-center justify-center">
@@ -55,7 +60,7 @@ export default function EventCard({
             placeholder="Event Description"
             cms
             textarea
-            name={`${title.split(" ")[0]}eventDescription`}
+            name={`events.${index}.eventDescription`}
           />
         </div>
         <div
@@ -63,9 +68,29 @@ export default function EventCard({
             !withoutBorder && "mb-[40px]"
           } `}
         >
-          <FillButton additionalStyle="pt-[12px] px-[40px] ">
-            add event
-          </FillButton>
+          <div
+            className={`flex items-center justify-center ${
+              !withoutBorder && "mb-[40px]"
+            } `}
+          >
+            <FillButton
+              additionalStyle="pt-[12px] px-[40px] gap-[10px]"
+              onClick={() => arrayHelpers.remove(index)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="24"
+                height="24"
+                fill="white"
+                viewBox="0 0 24 24"
+              >
+                <path d="M 10 2 L 9 3 L 4 3 L 4 5 L 5 5 L 5 20 C 5 20.522222 5.1913289 21.05461 5.5683594 21.431641 C 5.9453899 21.808671 6.4777778 22 7 22 L 17 22 C 17.522222 22 18.05461 21.808671 18.431641 21.431641 C 18.808671 21.05461 19 20.522222 19 20 L 19 5 L 20 5 L 20 3 L 15 3 L 14 2 L 10 2 z M 7 5 L 17 5 L 17 20 L 7 20 L 7 5 z M 9 7 L 9 18 L 11 18 L 11 7 L 9 7 z M 13 7 L 13 18 L 15 18 L 15 7 L 13 7 z"></path>
+              </svg>
+              Delete Event
+            </FillButton>
+          </div>
         </div>
       </div>
     </div>
