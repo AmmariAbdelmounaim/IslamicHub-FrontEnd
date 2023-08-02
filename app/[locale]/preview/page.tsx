35198e907dd79faaf15fc2238befa74f29f9b5a3
Ammari Abdelmounaim <abdelmounaim.ammari@gmail.com>
2023-08-02 09:57:52 +0100

    preview page

--- app/[locale]/preview/page.tsx
+++ app/[locale]/preview/page.tsx
@@ -1,20 +1,19 @@
 import React from "react";
 import Navbar from "./Navbar"; // Importer le composant Navbar
-import Slide from "./Slider"; // Importer le composant Slide
-import Percent from "./Percent"
-import Slider from "react-slick";
+import Percent from "./Percent";
+import SliderImage from "./Slider";
 
 const PreviewPage = () => {
-  return (  <div>
-    <Navbar />
-    {/*<Slider/>*/} 
-    <Percent />
-   
-    <div>
-      {/* Autres sections et éléments de contenu ici */}
-    </div>
-  </div>
-);
+  return (
+    <>
+      <Navbar />
+      <SliderImage />
+
+      <Percent />
+
+      <div>{/* Autres sections et éléments de contenu ici */}</div>
+    </>
+  );
 };
 
 export default PreviewPage;
