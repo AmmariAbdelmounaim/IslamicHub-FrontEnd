8395e82bc5c8991874d23b4f510b2a49de6243ba
ChaouiAli <a.echchaoui@esisa.ac.ma>
2023-08-02 20:12:31 +0100

    about us

--- app/[locale]/preview/AboutUs.tsx
+++ app/[locale]/preview/AboutUs.tsx
@@ -3,24 +3,22 @@ import React from "react";
 function AboutUsIslmacCenter() {
   return (
     <div className="flex flex-col gap-[40px] mx-[100px] py-[64px] border-b-[2px] border-b-secondary-brown-light-active ">
-      <h1>title</h1>
+        <h1 className="text-primary-orange-darker font-sourceSerif  text-[40px] font-normal flex justify-center items-center  ">
+        About Us
+      </h1>
       <div className="flex flex-col gap-[56px]">
         <div className="flex justify-between text-[40px]">
-          <h3>who we are</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit ab
-            alias magnam. Repudiandae, nemo. Voluptatem, illo perferendis sunt
-            deleniti sapiente explicabo. Expedita deleniti ipsum ipsam quisquam
-            tempora dolore nobis cupiditate.
+          <h3 className="text-primary-orange-darker font-sourceSerif  text-[32px] font-normal w-[197px] h-[40px] ">
+            Who We Are</h3>
+          <p className="text-[18px] font-normal font-poppins text-primary-orange-dark-active w-[707px]">
+          Welcome to Al Haramain Center, a vibrant Islamic haven dedicated to nurturing hearts, minds, and souls. Our sanctuary fosters spiritual growth, unity, and knowledge within the community. With a rich heritage and commitment to Islamic teachings, we offer enlightening lectures, engaging education, and uplifting social gatherings. Embracing diversity, we build bridges through interfaith dialogues and outreach. Seek inspiration, solace, and strengthen your connection to the Divine here. Join us on this fulfilling journey of compassion, knowledge, and service.
           </p>
         </div>
         <div className="flex justify-between">
-          <h3>our vision</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-            architecto magnam necessitatibus aut excepturi dignissimos iste
-            natus fuga accusamus commodi veniam voluptatibus quos minima at
-            esse, possimus eligendi eius mollitia.
+          <h3 className="text-primary-orange-darker font-sourceSerif  text-[32px] font-normal w-[197px] h-[40px]d">
+            our vision</h3>
+          <p className="text-[18px] font-normal font-poppins text-primary-orange-dark-active w-[707px]">
+          At Al Haramain Center, we believe in the power of unity and community. As a beacon of faith, we warmly welcome all to discover our diverse programs and services. Together, let us flourish in faith, knowledge, and harmony, making a lasting impact on individuals and the broader community. Join our harmonious space, and together, we shall thrive at Al Haramain Center.
           </p>
         </div>
       </div>
