fde7c9abcd1cd0e777c6b8cebe7b65df435bd474
Ammari Abdelmounaim <abdelmounaim.ammari@gmail.com>
2023-08-07 14:54:26 +0100

    adding theme and header footer interfaces

--- types/types.ts
+++ types/types.ts
@@ -17,4 +17,39 @@ export interface Center {
   address: string;
   ownerId: number;
   description: string;
+  themeDTO: Theme;
+  headerFooterDTO: HeaderFooter;
+}
+
+export interface Theme {
+  id: number;
+  titleColor: string;
+  textColor: string;
+  backgroundColor: string;
+  primaryColor: string;
+  centerId: number;
+}
+export interface HeaderFooter {
+  id: number;
+  headerBGColor: string;
+  headerTextColor: string;
+  headerAboutUs: boolean;
+  headerOurServices: boolean;
+  headerOurEvents: boolean;
+  headerPrayerTime: boolean;
+  headerTestimonials: boolean;
+  headerContanctUs: boolean;
+  largeLogo: string;
+  smallLogo: string;
+  footerBGColor: string;
+  footerTextColor: string;
+  footerFacebook: string;
+  footerInsta: string;
+  footerTwitter: string;
+  footerThreads: string;
+  footerEmail: string;
+  footerPhoneNumber: string;
+  footerAddress: string;
+  footerwtp: string;
+  centerId: number;
 }
