9f5eb2837d801e88f176bc7008611827582bf010
Ammari Abdelmounaim <abdelmounaim.ammari@gmail.com>
2023-08-14 10:45:59 +0100

    adapting navbar for preview

--- app/[locale]/preview/Navbar.tsx
+++ app/[locale]/preview/Navbar.tsx
@@ -2,64 +2,125 @@
 import Link from "next/link";
 import Image from "next/image";
 import FillButton from "../../../components/button/FillButton";
+import { useAppSelector } from "../../../redux/store";
 const Navbar = () => {
+  const { userInfo } = useAppSelector((state) => state.auth);
   return (
-    <div className="fixed top-0 right-0 left-0 flex px-[100px] bg-primary-orange-dark-50-opacity justify-between items-center z-50">
-      <div>
+    <div
+      className={`absolute top-0 right-0 left-0 flex px-[100px] 
+          "bg-primary-orange-dark-50-opacity"
+      }  justify-between items-center z-50`}
+      style={{
+        backgroundColor: `${userInfo?.centerDTO.headerFooterDTO.headerBGColor}`,
+      }}
+    >
+      <div className="h-[57px] w-[112px]">
         <Image
-          src={"/mockLogo.svg"}
+          src={`${
+            userInfo?.centerDTO.headerFooterDTO.largeLogo ?? "/mockLogo.svg"
+          }`}
           alt="mock logo for islamic center"
-          width={118}
+          width={112}
           height={57}
         />
       </div>
       <ul className="h-full gap-[33px] py-[22px] flex  items-center ">
         {" "}
-        {/* Ajouter la classe "flex" ici */}
-        <li>
-          <Link
-            className="no-underline  transition-all text-secondary-brown-light font-poppins capitalize text-[20px] hover:text-secondary-brown-light-hover"
-            href="#"
-          >
-            Prayer time
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="no-underline  transition-all text-secondary-brown-light font-poppins capitalize text-[20px] hover:text-secondary-brown-light-hover"
-            href="#"
-          >
-            About us
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="no-underline  transition-all text-secondary-brown-light font-poppins capitalize text-[20px] hover:text-secondary-brown-light-hover"
-            href="#"
-          >
-            Our services
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="no-underline  transition-all text-secondary-brown-light font-poppins capitalize text-[20px] hover:text-secondary-brown-light-hover"
-            href="#"
-          >
-            Our Events
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="no-underline  transition-all text-secondary-brown-light font-poppins capitalize text-[20px] hover:text-secondary-brown-light-hover"
-            href="#"
-          >
-            Contact Us
-          </Link>
-        </li>
-        <li className="h-full flex items-center ml-auto">
-          {" "}
-          {/* Utiliser la classe "ml-auto" ici */}
-        </li>
+        {userInfo?.centerDTO.headerFooterDTO.headerPrayerTime && (
+          <li>
+            <Link
+              className={`no-underline  transition-all
+              text-secondary-brown-light
+              font-poppins capitalize text-[20px] `}
+              style={{
+                color: `${userInfo.centerDTO.headerFooterDTO.headerTextColor}`,
+              }}
+              href="#"
+            >
+              Prayer time
+            </Link>
+          </li>
+        )}
+        {userInfo?.centerDTO.headerFooterDTO.headerAboutUs && (
+          <li>
+            <Link
+              className={`no-underline  transition-all ${
+                userInfo.centerDTO.headerFooterDTO.headerTextColor ??
+                "text-secondary-brown-light"
+              } font-poppins capitalize text-[20px] `}
+              style={{
+                color: `${userInfo.centerDTO.headerFooterDTO.headerTextColor}`,
+              }}
+              href="#"
+            >
+              About us
+            </Link>
+          </li>
+        )}
+        {userInfo?.centerDTO.headerFooterDTO.headerOurServices && (
+          <li>
+            <Link
+              className={`no-underline  transition-all ${
+                userInfo.centerDTO.headerFooterDTO.headerTextColor ??
+                "text-secondary-brown-light"
+              } font-poppins capitalize text-[20px] `}
+              style={{
+                color: `${userInfo.centerDTO.headerFooterDTO.headerTextColor}`,
+              }}
+              href="#"
+            >
+              Our services
+            </Link>
+          </li>
+        )}
+        {userInfo?.centerDTO.headerFooterDTO.headerOurEvents && (
+          <li>
+            <Link
+              className={`no-underline  transition-all ${
+                userInfo.centerDTO.headerFooterDTO.headerTextColor ??
+                "text-secondary-brown-light"
+              } font-poppins capitalize text-[20px] `}
+              style={{
+                color: `${userInfo.centerDTO.headerFooterDTO.headerTextColor}`,
+              }}
+              href="#"
+            >
+              Our Events
+            </Link>
+          </li>
+        )}
+        {userInfo?.centerDTO.headerFooterDTO.headerContanctUs && (
+          <li>
+            <Link
+              className={`no-underline  transition-all ${
+                userInfo.centerDTO.headerFooterDTO.headerTextColor ??
+                "text-secondary-brown-light"
+              } font-poppins capitalize text-[20px] `}
+              style={{
+                color: `${userInfo.centerDTO.headerFooterDTO.headerTextColor}`,
+              }}
+              href="#"
+            >
+              Contact Us
+            </Link>
+          </li>
+        )}
+        {userInfo?.centerDTO.headerFooterDTO.headerTestimonials && (
+          <li>
+            <Link
+              className={`no-underline  transition-all ${
+                userInfo.centerDTO.headerFooterDTO.headerTextColor ??
+                "text-secondary-brown-light"
+              } font-poppins capitalize text-[20px] `}
+              style={{
+                color: `${userInfo.centerDTO.headerFooterDTO.headerTextColor}`,
+              }}
+              href="#"
+            >
+              Testimonials
+            </Link>
+          </li>
+        )}
       </ul>
       <FillButton>donation</FillButton>
     </div>
