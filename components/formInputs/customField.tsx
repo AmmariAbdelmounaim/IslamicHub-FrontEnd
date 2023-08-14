16765bc21f641b4fe4922e8d304db60d36ca2176
Ammari Abdelmounaim <abdelmounaim.ammari@gmail.com>
2023-08-14 10:51:11 +0100

    cleaning the code

--- components/formInputs/customField.tsx
+++ components/formInputs/customField.tsx
@@ -8,48 +8,50 @@ interface CustomFieldProps<T> {
   type?: string;
   cms?: boolean;
   textarea?: boolean;
+  value?: string; // Consider if this prop is really needed
 }
 
 export function CustomField<T>({
   name,
   label,
   placeholder,
-  type,
+  type = "text",
   cms = false,
   textarea = false,
 }: CustomFieldProps<T>) {
-  const { errors, touched } = useFormikContext<T>(); // access formik context
+  const { errors, touched } = useFormikContext<T>();
   const hasError = Boolean(touched[name as keyof T] && errors[name as keyof T]);
 
+  const fieldClassNames = `
+    w-full 
+    ${textarea ? "h-[100px] pt-[10px] pl-[10px]" : "h-[40px] pl-[16px]"} 
+    items-center rounded-lg 
+    ${!cms ? "min-w-[420px]" : ""}
+    font-poppins text-[16px] font-medium placeholder:text-color-placeholder bg-transparent
+    ${
+      hasError
+        ? "border-2 border-red-600"
+        : "border-2 border-secondary-brown-normal "
+    }
+  `;
+
   return (
-    <div className={`${cms && "flex flex-col justify-center w-[366px]"}`}>
+    <div className={`${cms ? "flex flex-col justify-center w-[366px]" : ""}`}>
       <label
         htmlFor={name as string}
-        className={` ${
-          cms && "text-center mb-[16px]"
+        className={`${
+          cms ? "text-center mb-[16px]" : ""
         } font-poppins capitalize text-[18px] text-secondary-brown-dark`}
       >
         {label}
       </label>
       <ErrorMessage name={name as string} />
       <Field
-        as={`${textarea ? "textarea" : "input"}`}
-        type={type || "text"}
+        as={textarea ? "textarea" : "input"}
+        type={textarea ? undefined : type} // avoid passing type to textarea
         name={name}
         placeholder={placeholder}
-        className={`
-          w-full flex  ${
-            textarea ? "h-[100px] pt-[10px] pl-[10px]" : "h-[40px] pl-[16px]"
-          }
-           items-center rounded-lg ${
-             !cms && "min-w-[420px]"
-           } font-poppins text-[16px] font-medium placeholder:text-color-placeholder bg-transparent
-          ${
-            hasError
-              ? "border-2 border-red-600"
-              : "border-2 border-secondary-brown-normal "
-          }
-        `}
+        className={fieldClassNames}
       />
     </div>
   );
